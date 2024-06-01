4fd6baa108114695102cb109e80a153e3de7e011
afidzulinn <afidzulin89@gmail.com>
2024-06-01 13:28:46 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
+ base: "/webapp",
  plugins: [react()],
  preview: {
+  host: "0.0.0.0",
   port: 8901,
   strictPort: true,
  },
  server: {
   port: 8901,
   strictPort: true,
-  host: "true",
-  origin: "http://0.0.0.0:8901",
+  host: "0.0.0.0",
+  // origin: "http://0.0.0.0:8901",
  },
+    build: {
+        chunkSizeWarningLimit: 5000,
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
